b45adb1c615ad890694cf3b15f68b54148f2b707
Kalenchenko Gleb <gkalenchenko@gmail.com>
2022-11-24 18:14:15 +0300

    adds final edits to task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -22,4 +22,3 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
-console.log(lightbox.captionType);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new vimeo(iframe);
+const localObj = localStorage.getItem('videoplayer-current-time');
+
+function callback(timePlayed) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(timePlayed));
+}
+
+player.on('timeupdate', throttle(callback, 1000));
+player.setCurrentTime(JSON.parse(localObj).seconds);
